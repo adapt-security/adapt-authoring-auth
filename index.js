2f439e2aff2d47c326670df62ef3a9598052ea77
Thomas Taylor <thomas.taylor@kineo.com>
2020-04-01 14:03:35 +0100

    Add export

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   AuthError: require('./lib/authError'),
+  AuthToken: require('./lib/authToken'),
   AuthUtils: require('./lib/authUtils'),
   Module: require('./lib/authModule')
 };
