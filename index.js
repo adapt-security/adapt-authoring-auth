c3fdcbfbac82566a2e32b156a7132942253b637b
Tom Taylor <hello@tomtaylor.name>
2021-11-17 19:20:20 +0000

    Refactor as ES Modules

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  AbstractAuthModule: require('./lib/AbstractAuthModule'),
-  AuthError: require('./lib/AuthError'),
-  AuthToken: require('./lib/AuthToken'),
-  AuthUtils: require('./lib/AuthUtils'),
-  Module: require('./lib/AuthModule')
-};
+export { default as AbstractAuthModule } from './lib/AbstractAuthModule';
+export { default as AuthError } from './lib/AuthError';
+export { default as AuthToken } from './lib/AuthToken';
+export { default as AuthUtils } from './lib/AuthUtils';
+export { default as Module } from './lib/AuthModule';
