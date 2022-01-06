18b92d6fc832637274854c58606768da1270767c
Tom Taylor <hello@tomtaylor.name>
2022-01-06 17:09:36 +0000

    Refactor module to use new errors API

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 export { default as AbstractAuthModule } from './lib/AbstractAuthModule.js';
-export { default as AuthError } from './lib/AuthError.js';
 export { default as AuthToken } from './lib/AuthToken.js';
 export { default as AuthUtils } from './lib/AuthUtils.js';
 export { default } from './lib/AuthModule.js';
