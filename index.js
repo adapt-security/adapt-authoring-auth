9fbca99c05d9ce3425caade032984f96b897010e
Thomas Taylor <hello@tomtaylor.name>
2023-03-20 13:21:47 +0000

    Enable eslint and standard ruleset

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-/** 
+/**
  * Authentication
  * @namespace auth
  */
-export { default as AbstractAuthModule } from './lib/AbstractAuthModule.js';
-export { default as AuthToken } from './lib/AuthToken.js';
-export { default as AuthUtils } from './lib/AuthUtils.js';
-export { default } from './lib/AuthModule.js';
+export { default as AbstractAuthModule } from './lib/AbstractAuthModule.js'
+export { default as AuthToken } from './lib/AuthToken.js'
+export { default as AuthUtils } from './lib/AuthUtils.js'
+export { default } from './lib/AuthModule.js'
